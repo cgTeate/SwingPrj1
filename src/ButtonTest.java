fe9db0fa124e28bc1ceb08e18c47cc07b27a459b
Calvin Teater <laughingglint3@gmail.com>
2022-08-25 11:14:07 -0500

    add documentation to main

--- src/ButtonTest.java
+++ src/ButtonTest.java
@@ -8,13 +8,21 @@ public class ButtonTest extends JFrame {
     private JButton button2;
     public ButtonTest() {
         super("ButtonTest");
-
+        /*
+         * Step 2. Create an instance of the private nested 
+         * class in the constructor where you are building 
+         * the UI.
+         */
         ActionHandler ah = new ActionHandler();
 
         JPanel buttonPanel=new JPanel();
         add(buttonPanel);
 
         button1=new JButton("Button 1");
+        /*
+         * Step 3. Add the handler to the component 
+         * for which it is to handle events.
+         */
         button1.addActionListener(ah);
         buttonPanel.add(button1);
 
@@ -27,7 +35,12 @@ public class ButtonTest extends JFrame {
         setVisible(true);
 
     }
-
+        /*
+         * Step 1:
+         * Create a private nested class that implements a listener 
+         * interface or extends an adapter class. The code in this 
+         * class tells the handler what to do when the event occurs.
+         */
     private class ActionHandler implements ActionListener
         {
             public void actionPerformed(ActionEvent e)
@@ -40,8 +53,8 @@ public class ButtonTest extends JFrame {
             {
               System.out.println("Button 2 clicked");
             }
-} 
-}
+        } 
+        }
     public static void main(String[] args) {
         new ButtonTest();
         
