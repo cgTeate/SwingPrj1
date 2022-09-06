06b0c697cb2dd5a66dbe6db3d9d24402878ff380
Calvin Teater <laughingglint3@gmail.com>
2022-09-06 18:36:51 -0500

    prj2

--- src/Project02.java
+++ src/Project02.java
@@ -1,11 +1,9 @@
 import java.awt.*;
 import java.awt.event.*;
-
-import javax.imageio.ImageIO;
-import javax.swing.*;
-
-import java.io.*;
 import java.awt.image.*;
+import javax.swing.*;
+import javax.imageio.*;
+import java.io.*;
 
 
 
@@ -15,10 +13,14 @@ public class Project02 extends JFrame {
         private int[] by;
         private int[] sx;
         private int[] sy;
-        private BufferedImage redBallBI;
-        private int selected=-1;
+        private int[] xoff;
+        private int[] yoff;
         private int[] origx;
         private int[] origy;
+        private BufferedImage redBallBI;
+        private int selected=-1;
+        private boolean[] gone;
+        private int[] ia;
 
 
     public Project02() {
@@ -27,7 +29,7 @@ public class Project02 extends JFrame {
         MouseHandler mh = new MouseHandler();
         MouseMotionHandler mmh = new MouseMotionHandler();
         
-        GamePanel gamePanel=new GamePanel();
+        gamePanel=new GamePanel();
         gamePanel.setPreferredSize(new Dimension(280,300));
         gamePanel.addMouseListener(mh);
         gamePanel.addMouseMotionListener(mmh);
@@ -38,6 +40,10 @@ public class Project02 extends JFrame {
         by=new int[10];
         sx=new int[10];
         sy=new int[10];
+        origx=new int[10];
+        origy=new int[10];
+        xoff=new int[10];
+        yoff=new int[10];
 
         for(int i=0; i<bx.length; i++){
             bx[i]=40+(i%5)*50;
@@ -63,42 +69,99 @@ public class Project02 extends JFrame {
         public void paintComponent(Graphics g) {
             super.paintComponent(g);
 
-            g.setColor(Color.WHITE);
-
+            //draws the squares at the center of each
             for(int i=0;i<bx.length;i++){
-                g.drawImage(redBallBI,bx[i]-20,by[i]-20,null);
-                g.drawString(""+i,bx[i]-3,by[i]+4);
                 g.setColor(Color.BLACK);
                 g.fillRect(sx[i]-20,sy[i]-20,40,40);
+                g.setColor(Color.WHITE);
+                g.drawString(""+i,sx[i]-3,sy[i]+4);
+            }
+            //draws the red balls at the cneter of each
+            for(int i=0;i<bx.length;i++){
+                //if(gone[i]) continue;
+                if(i==selected)continue;
+                g.drawImage(redBallBI,bx[i]-20,by[i]-20,null);
+                g.setColor(Color.WHITE);
+                g.drawString(""+i,bx[i]-3,by[i]+4);
+            }
+            //draws selected ball over the others 
+            if(selected!=-1){
+                g.drawImage(redBallBI,bx[selected]-20,by[selected]+-20,null);
+                g.setColor(Color.WHITE);
+                g.drawString(""+selected,bx[selected]-3,by[selected]+4);
             }
             
         }
     }
 
+    // private class AnimationThread extends Thread{
+    //     private int index;
+    //     private float startx;
+    //     private float starty;
+    //     private float endx;
+    //     private float endy;
+    //     private int steps;
+    //     private float deltax;
+    //     private float deltay;
+
+    //     public AnimationThread(int index, float startx, float starty, float endx, float endy) { 
+    //         this.index=index;
+    //         this.startx=startx;
+    //         this.starty=starty;
+    //         this.endx=index;
+    //         this.index=index;
+    //         steps=((int)Math.sqrt((endx-startx)*(endx-startx)+(endy-starty)*(endy-starty))/10);
+    //         deltax=(endx-startx)/steps;
+    //         deltay=(endy-starty)/steps;
+    //     }
+
+    //     public void run(){
+    //         try {
+    //             for(int i=0;i<steps;i++){
+    //                 sleep(20);
+    //                 bx[index]+=deltax;
+    //                 by[index]+=deltay;
+    //                 gamePanel.repaint();
+    //             }
+                
+    //         } catch (InterruptedException ie) {
+                
+    //         }
+    //     }
+    // }
+
     private class MouseHandler extends MouseAdapter 
     {
         public void mousePressed(MouseEvent e) 
         {
-
+            //tells which ball is selected
             for(int i=0;i<bx.length;i++){
                 if(Math.abs(e.getX()-bx[i])<20 && Math.abs(e.getY()-by[i])<20)
                 {
+                    xoff[i]=e.getX()-bx[i];
+                    yoff[i]=e.getY()-by[i];
                     selected=i;
                 }
+                System.out.println(bx[i]);
             }
         }
+        public void mouseReleased(MouseEvent e) {
+           
+            //resets selected whe mouse is released
+            selected=-1;
+            //reset the position of the ball once released
+            //origx[selected]=origx[];
+            //origy[selected]=origy[selected];
+        }
     }
-    private void mouseReleased(MouseEvent e) {
-        selected=-1;
-    }
-
 
     private class MouseMotionHandler extends MouseMotionAdapter {
         public void mouseDragged(MouseEvent e) {
             if(selected==-1) return;
-            bx[selected]=e.getX(); 
-            by[selected]=e.getX(); 
-
+            //bx[selected]=e.getX()-xoff[selected]; 
+            //by[selected]=e.getY()-yoff[selected];
+            bx[selected]=e.getX();
+            by[selected]=e.getY();
             gamePanel.repaint();
 
     }
@@ -108,4 +171,5 @@ public class Project02 extends JFrame {
         new Project02();
     }
 }
+
 
